48b3f51b551449b51433d050c32e668a15c2de95
AnthonyCxc <AnthonyCxc95@gmail.com>
2017-11-04 10:42:12 +0800

    4/11/2017 10:42am

--- db/migrate/20171023035903_create_reservations.rb
+++ db/migrate/20171023035903_create_reservations.rb
@@ -1,7 +1,6 @@
 class CreateReservations < ActiveRecord::Migration[5.1]
   def change
     create_table :reservations do |t|
-    	t.boolean :admin, null: false, default: false
 
       t.timestamps
     end
